f2e025518a6f496491a35eece0e9f97a55f008f9
Dibash Sapkota _ <92871130+SapkotaDibash@users.noreply.github.com>
2022-09-17 21:38:01 +0545

    Add files via upload

---
+++ demo/next/static/9b8nYPBqLrWLIcDNdRvtS/_buildManifest.js
@@ -0,0 +1,10 @@
+self.__BUILD_MANIFEST = {
+    __rewrites: {
+        beforeFiles: [],
+        afterFiles: [],
+        fallback: []
+    },
+    "/": ["static/chunks/417-00d7d0efd22d3768.js", "static/chunks/pages/index-8683ca2b2fc6eaa4.js"],
+    "/_error": ["static/chunks/pages/_error-0a004b8b8498208d.js"],
+    sortedPages: ["/", "/_app", "/_error"]
+}, self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
