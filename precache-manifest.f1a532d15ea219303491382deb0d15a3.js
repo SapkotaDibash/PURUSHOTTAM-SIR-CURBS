542433f7f941a1c46b3d0676c3614f60d9430eb9
Dibash Sapkota _ <92871130+SapkotaDibash@users.noreply.github.com>
2022-09-24 19:58:57 +0545

    Add files via upload

---
+++ precache-manifest.f1a532d15ea219303491382deb0d15a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bfc5e643560d3d3042e",
+    "url": "/static/css/main.f86b9b49.chunk.css"
+  },
+  {
+    "revision": "2bfc5e643560d3d3042e",
+    "url": "/static/js/main.2bfc5e64.chunk.js"
+  },
+  {
+    "revision": "871e630e23fe57207b02",
+    "url": "/static/js/1.871e630e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b8f177cdc15b9b5d51cd568c611bb231",
+    "url": "/index.html"
+  }
+];
