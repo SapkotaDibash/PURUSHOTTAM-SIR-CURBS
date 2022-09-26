275d0e31b2ce2eda20dc7022d1109aa0e67f19f9
Dibash Sapkota _ <92871130+SapkotaDibash@users.noreply.github.com>
2022-09-26 21:19:39 +0545

    google adsenc added, few changes made

---
+++ precache-manifest.8b0b362c5fe0c50329531877ebeff22a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bd99d34692dbcbcc4c4",
+    "url": "/static/css/main.f86b9b49.chunk.css"
+  },
+  {
+    "revision": "0bd99d34692dbcbcc4c4",
+    "url": "/static/js/main.0bd99d34.chunk.js"
+  },
+  {
+    "revision": "871e630e23fe57207b02",
+    "url": "/static/js/1.871e630e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a3df69b4d4e708d206ce2ed934ac3ea1",
+    "url": "/index.html"
+  }
+];
